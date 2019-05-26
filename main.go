2dc452bdbe9abf1b6adce7da5e582710242a9002
codeallthethingz <howdoicontactwill@gmail.com>
2019-05-26 15:02:04 -0700

    wip wasm

--- main.go
+++ main.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gopherjs/gopherwasm/js"
 )
 
-func sayHi(args []js.Value) {
-	fmt.Printf("%s\n", args[0].String())
-	js.Global().Call("facetEngineCallback", "one,two,three")
+var facetGroups *FacetGroups
+var query = &Query{}
+
+// JSQuery wasm interface to query the facet groups
+func JSQuery(args []js.Value) {
+	fmt.Println("query called")
+	ids, err := facetGroups.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	idsByets, err := json.Marshal(ids)
+	if err != nil {
+		panic(err)
+	}
+
+	js.Global().Call("facetEngineCallbackRecords", string(idsByets))
+}
+
+// JSInitializeObjects wasm interface to take the data and parse out the facets
+func JSInitializeObjects(args []js.Value) {
+	fmt.Println("facetEngineInitializeObjects called")
+	configString := args[0].String()
+	dataJSON := args[1].String()
+
+	fmt.Println("Config: " + configString)
+	fmt.Println("Data: " + dataJSON)
+
+	facetPath := &FacetPath{}
+	err := json.Unmarshal([]byte(configString), facetPath)
+	if err != nil {
+		panic(err)
+	}
+	facetGroups, err = CreateFacetGroups(dataJSON, facetPath)
+	if err != nil {
+		panic(err)
+	}
+	facetGroupsBytes, err := json.Marshal(facetGroups)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("facets: " + string(facetGroupsBytes))
+	js.Global().Call("facetEngineCallbackFacets", string(facetGroupsBytes))
 }
 
 func registerCallbacks() {
-	js.Global().Set("sayHi", js.NewCallback(sayHi))
+	js.Global().Set("facetEngineInitializeObjects", js.NewCallback(JSInitializeObjects))
+	js.Global().Set("facetEngineQuery", js.NewCallback(JSQuery))
 }
 
 func main() {
